03bf0517a79e45760be1ca7047f951c22c2d810c
Luis De Siqueira <luis@pleme.io>
2023-01-19 21:15:41 -0300

    ramix

--- modules/modules.nix
+++ modules/modules.nix
@@ -44,6 +44,7 @@ let
     ./misc/xdg-user-dirs.nix
     ./misc/xdg.nix
     ./misc/xfconf.nix
+    ./raimix/programs/neovim.nix
     ./programs/abook.nix
     ./programs/aerc.nix
     ./programs/afew.nix
