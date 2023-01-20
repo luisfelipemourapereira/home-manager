9afd9bb466e1a047acb22c8c4ebc25c092474c07
Luis De Siqueira <luis@pleme.io>
2023-01-19 21:02:14 -0300

    modules: add raimix namespace

---
+++ modules/raimix/programs/neovim.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.raimix.programs.neovim;
+
+in
+{
+  options = {
+    enable = mkEnableOption "neovim";
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.neovim-unwrapped;
+      description = lib.mdDoc ''
+        neovim derivation
+      '';
+    };
+  };
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      # config.home.packages = [ cfg.package ];
+    })
+  ];
+}
